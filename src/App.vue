704956e23d22fefa3796fc3b28acf2d8aa961f24
LelouchFR <baptistezahnow@gmail.com>
2023-03-21 20:55:31 +0100

    Created the Card and put them correctly with grid

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import Card from './components/Card.vue';
+</script>
+
+<template>
+    <div class="container">
+        <Card Card="card" CardNumber="card1" image="img/Angular.png" title="Angular"/>
+        <Card Card="card" CardNumber="card2" image="img/NodeJs.jpg" title="Node Js"/>
+        <Card Card="card" CardNumber="card3" image="img/Preact.png" title="Preact"/>
+        <Card Card="card" CardNumber="card4" image="img/React.png" title="React"/>
+        <Card Card="card" CardNumber="card5" image="img/Svelte.png" title="Svelte"/>
+        <Card Card="card" CardNumber="card6" image="img/Vue.jpg" title="Vue"/>
+    </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :class="Card, CardNumber">
+        <img :src="image" :alt="title">
+        <h3>{{ title }}</h3>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        Card: String,
+        CardNumber: String,
+        image: String,
+        title: String,
+    });
+</script>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
