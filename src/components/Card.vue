5943ad568148468266112995e9355006abb870c3
LelouchFR <baptistezahnow@gmail.com>
2023-03-23 00:05:54 +0100

    CardSystem added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,6 +18,26 @@
 </template>
 
 <script>
+    let FlipNums = 0;
+    let foundPairs = 0;
+    let CardPair = [];
+    let CardPairTurner = [];
+
+    function wait(time) {
+        return new Promise(resolve => {setTimeout(resolve, time)});
+    }
+
+    function CheckCards(FirstCard, SecondCard, firstTurn, secondTurn) {
+        if (FirstCard === SecondCard) {
+            foundPairs++;
+        } else {
+            firstTurn.isFlipped = !firstTurn.isFlipped;
+            secondTurn.isFlipped = !secondTurn.isFlipped;
+        }
+        CardPair = [];
+        CardPairTurner = [];
+    }
+    
     export default {
         name: "Card",
         data() {
@@ -27,6 +47,13 @@
         },
         methods: {
             flipCard() {
+                CardPair.push(this.image);
+                CardPairTurner.push(this);
+                FlipNums++;
+                if (FlipNums === 2) {
+                    CheckCards(CardPair[0], CardPair[1], CardPairTurner[0], CardPairTurner[1]);
+                    FlipNums = 0;
+                }
                 this.isFlipped = !this.isFlipped;
             },
         },
